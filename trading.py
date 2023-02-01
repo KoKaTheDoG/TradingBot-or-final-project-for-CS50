4aa4ba0e9d76f8dee3e1f8175e2710c3141e6d8a
KoKaTheDoG <ossgetlost@gmail.com>
2023-02-01 13:27:07 +0200

    add db, realiz function build chains but not good

--- trading.py
+++ trading.py
@@ -53,12 +53,9 @@ def create_binance_tikers_table():
     db.executemany("INSERT INTO binance_tikers (tiker, master, satelite) VALUES(:symbol, :master, :satelite)", data)
     connection.commit()
     
-    connection.close()
-      
-    return True
-    
 
 def price_request():
+
     data = db.execute("SELECT id, tiker FROM binance_tikers").fetchall()
     data = [{"id": item[0], "tiker": item[1]} for item in data]
 
@@ -71,4 +68,33 @@ def price_request():
         query = db.execute(query, (item["id"], price_request["price"]))
 
 
-def temp():
+def create_chains_table():
+
+    data = db.execute("SELECT master,satelite FROM binance_tikers GROUP BY master").fetchall()   
+ 
+    result = [{"start": item[0], "traces":[{"trace": item[0] + "-" + item[1], "position": item[1], "iteration": 0}]} for item in data]
+
+    for item in result:
+        for temp_trace in item["traces"]:
+          
+            if temp_trace["iteration"] < 5:
+                query = db.execute("SELECT satelite FROM binance_tikers WHERE master = ?", (temp_trace["position"],))
+                query = [temp[0] for temp in query] 
+                now = temp_trace
+                now_index = item["traces"].index(temp_trace)
+                
+                for value in query: 
+                    item["traces"].append({"trace": now["trace"] + "-" + value, "position": value, "iteration": temp_trace["iteration"] + 1})
+
+                item["traces"].pop(now_index)
+            
+    for item in result:
+        for val in item["traces"]:
+           
+            query = ("INSERT INTO binance_chains (chain) VALUES (?)")
+            query = db.execute(query, (val["trace"],))
+            connection.commit()
+
+
+create_chains_table()
+connection.close()
